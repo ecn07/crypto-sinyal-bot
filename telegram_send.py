593de8fbd320c01059c509ff25b760456e685a99
ecn07 <psenem4@gmail.com>
2025-06-01 03:17:49 +0300

    Update telegram_send.py

--- telegram_send.py
+++ telegram_send.py
@@ -1,9 +1,12 @@
 import os
+import asyncio
 from telegram import Bot
 
+# Token ve Chat ID environment variables'dan okunuyor
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = int(os.getenv("TELEGRAM_CHAT_ID"))
+
 bot = Bot(token=TOKEN)
 
 async def send_telegram_message(message):
-    await bot.send_message(chat_id=CHAT_ID, text=message)
+    await bot.send_message(chat_id=CHAT_ID, text=message, parse_mode='Markdown')
